ca4365a6134eab218787c67f24b8dc512525caa2
René Müller <rene.mueller@smail.inf.h-brs.de>
2019-07-14 15:33:35 +0200

    edit index.js

--- mongo_connector/index.js
+++ mongo_connector/index.js
@@ -6,7 +6,8 @@ const app = express();
 const config = {
     mongo_host: '172.17.0.3',
     mongo_port: 27017,
-    mongo_db: 'mongo_connector',
+    mongo_db: 'ccm',
+    mongo_collection: 'rmuel12s_components',
     server_port: 3000
 };
 
@@ -18,7 +19,7 @@ mongo.MongoClient.connect (
     (err, client) => {
 
         if (!err)
-            db = client.db ('ccm');
+            db = client.db (config.mongo_db);
         else
             console.log (err);
 });
@@ -27,7 +28,7 @@ app.get ('/time/:id', (req, res) => {
 
     hrtime_start = process.hrtime();
 
-    db.collection ('rmuel12s_components').findOne ({_id: req.params.id}, (err, result) => {
+    db.collection (config.mongo_collection).findOne ({_id: req.params.id}, (err, result) => {
 
         if (err) throw err;
 
